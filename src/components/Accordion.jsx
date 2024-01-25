e90313b315f2a0dfc2ff25990fb8e510fd432596
Deepak kumar <106810862+deepakkumar182002@users.noreply.github.com>
2024-01-25 23:24:43 +0530

    error resolve ...

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import "../App.css";
 
-const Accordion = (props,{heading, content}) => {
+const Accordion = ({heading, content}) => {
   const [isActive, setIsActive] = useState(false);
   return (
     <>
     
     <li className="accordion-item"style={{ background:"rgb(248 249 250)", width:"100%"}}>
       <div className="accordion-toggle" style={{margin:"5px", border:"1px solid #e7e6e6", width:"auto", scale:"auto"}} onClick={() => setIsActive(!isActive)}>
-        <h3 style={{fontSize:"1.2rem"}}>{props.heading}</h3><span>{isActive ? "-" : "+"}</span>
+        <h3 style={{fontSize:"1.2rem"}}>{heading}</h3><span>{isActive ? "-" : "+"}</span>
       </div>
-      {isActive && <div className="accordion-content">{props.content}</div>}
+      {isActive && <div className="accordion-content">{content}</div>}
     </li>
     </>
   );
